e2198a29ccff4a481e37bed2addf24619bb4097d
dylanwroberts <dylanwroberts99@gmail.com>
2019-11-20 12:38:05 -0600

    finished handlers

--- route-handlers/incidentsHandler.js
+++ route-handlers/incidentsHandler.js
@@ -1,6 +1,4 @@
-const xmlconverter = require('js2xmlparser');
 const sqlHelper = require('../db/sql-helper');
-const dateHelper = require('compare-dates');
 /** @todo send back all incidents when no query parameters */
 /** @todo start_date - first date to include in results (e.g. ?start_date=09-01-2019) */
 /** @todo end_date - last date to include in results (e.g. ?end_date=10-31-2019) */
@@ -9,22 +7,33 @@ const dateHelper = require('compare-dates');
 /** @todo neighborhood - comma separated list of neighborhood numbers to include in result (e.g. ?id=11,14). By default all neighborhoods should be included. */
 /** @todo limit - Result should include the N most recent incidents */
 const incidentsHandler = (db, req, res) => {
-  const limit = req.query.limit < 10000 ? req.query.limit : 10000;
+  let date_condition = '';
+  let params = [];
 
-  //'T23:59:59' is appended to each query so that it will grab all incidents during that specific date
-  let startDate = req.query.start_date
-    ? `${req.query.start_date}T23:59:59`
-    : '2014-08-14T00:00:00';
+  //check for what the user provided and create the query based on it
+  if (req.query.start_date && req.query.end_date) {
+    params.push(req.query.start_date);
+    params.push(req.query.end_date);
+    date_condition = 'WHERE date_time BETWEEN (?) AND (?)';
+  } else if (req.query.start_date) {
+    params.push(req.query.start_date);
+    date_condition = 'WHERE date_time >= (?)';
+  } else if (req.query.end_date) {
+    params.push(req.query.end_date);
+    date_condition = 'WHERE date_time <= (?)';
+  }
 
-  let endDate = req.query.end_date
-    ? `${req.query.end_date}T23:59:59`
-    : '2019-10-30T23:57:08.000';
+  //push limit onto the parameter array - 10K default and fence
+  params.push(req.query.limit < 10000 ? req.query.limit : 10000);
 
-  let query = `SELECT * FROM Incidents WHERE date_time BETWEEN (?) AND (?) ORDER BY date_time DESC LIMIT (?)`;
+  let query = `SELECT * FROM Incidents ${date_condition} ORDER BY date_time DESC LIMIT (?)`;
 
-  db.all(query, [startDate, endDate, limit], (err, data) => {
-    res.send(data);
-    console.log(err);
+  db.all(query, params, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
   });
 };
 
